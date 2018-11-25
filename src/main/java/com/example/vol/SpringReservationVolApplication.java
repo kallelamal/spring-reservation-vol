f9813d54f53f5e71c0ffb03f3dbe442f4e2a1d96
Amal kallel <amal.kallel94@gmail.com>
2018-11-25 16:32:55 +0100

    initial commit spring project

---
+++ src/main/java/com/example/vol/SpringReservationVolApplication.java
@@ -0,0 +1,12 @@
+package com.example.vol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringReservationVolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringReservationVolApplication.class, args);
+    }
+}
---
+++ src/test/java/com/example/vol/SpringReservationVolApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.vol;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringReservationVolApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
